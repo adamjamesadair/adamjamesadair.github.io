1d168ce3a916480bbfe4edb5e25d9e5bc88592e3
adamjamesadair <adam.james.adair@gmail.com>
2018-07-10 17:29:20 +0900

    Added mobile support

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,10 @@
-var navElements = [$('#nav-home'), $('#nav-experience'), $('#nav-projects')];
+var navElements = [$('#nav-home'), $('#nav-experience'), $('#nav-projects'), $('#nav-contact')];
 $(window).on("scroll", function(event) {
   var scrollPos = window.pageYOffset;
-  [$('#home'), $('#experience'), $('#projects')].forEach((refElement, i) => {
+  [$('#home'), $('#experience'), $('#projects'), $('#contact')].forEach((refElement, i) => {
     if (refElement.offset().top <= scrollPos + 10 && refElement.offset().top + refElement.height() > scrollPos - 100) {
       $('.nav-link').removeClass("active");
       navElements[i].addClass("active");
-    } else {
-      //currLink.removeClass("window-select-active");
-      //currLink.addClass("window-select-btn");
     }
   });
 });
